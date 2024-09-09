4f7b178e67a35ec3beb097dd9a7e0f847a64ef54
gaetanodigrazia <gaetano.digrazia@bitrock.it>
2024-09-09 23:17:36 +0200

    first commit

---
+++ src/main/java/com/digrazia/sample_apache_kafka_flink/SampleApacheKafkaFlinkApplication.java
@@ -0,0 +1,13 @@
+package com.digrazia.sample_apache_kafka_flink;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SampleApacheKafkaFlinkApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SampleApacheKafkaFlinkApplication.class, args);
+    }
+
+}
